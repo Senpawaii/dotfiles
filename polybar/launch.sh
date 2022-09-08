0d87773d33f05f63438827cb5a078325b3ba5533
Senpawaii <joao.miguel.queiros@gmail.com>
2022-09-08 13:06:01 +0100

    Initial commit with polybar configs

---
+++ polybar/launch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+echo "---" | tee -a /tmp/polybar1.log
+
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+else
+  primary=DP-1-0
+
+  for m in $screens; do
+    if [[ $primary == $m ]]; then
+        MONITOR=$m TRAY_POS=right polybar main &
+    else
+        MONITOR=$m TRAY_POS=none polybar secondary &
+    fi
+  done
+fi
+
+echo "Polybar launched..."
